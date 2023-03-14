a1d0522e165f967ad0e365f777750d1c05816842
Max Alterot <101337822+alterot@users.noreply.github.com>
2023-03-14 21:06:45 +0100

    add DOM elements for result as well as game end and reset button

--- javascript.js
+++ javascript.js
@@ -7,6 +7,9 @@ const displayWins = document.getElementById('wins');
 const displayLosses = document.getElementById('losses');
 const displayDraws = document.getElementById('draws');
 const displayRounds = document.getElementById('rounds');
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', reset);
 
 let rounds = 0;
 let wins = 0;
@@ -19,26 +22,39 @@ buttons.forEach(button => {
         const playerChoice = button.id.toUpperCase();
         const computerChoice = getComputerChoice();
         const result = playRound (playerChoice, computerChoice);
+        let resultMessage = '';
 
         rounds ++;
         if (result === 'draw') {
             draws ++;
+            resultMessage = `Oh you both chose ${playerChoice}, it\'s a draw mate!`;
         } else if (result === 'win') {
             wins ++;
+            resultMessage = `DANG! You chose ${playerChoice}, and computer chose ${computerChoice}, you frikkin won the round!`;
         } else {
             losses ++;
+            resultMessage = `No no bro ${playerChoice} sucks against ${computerChoice}, you lost the round!.`;
         }
 
-        displayResult.textContent = result;   
+        displayResult.textContent = resultMessage;   
         displayWins.textContent = wins;
         displayLosses.textContent = losses;
         displayDraws.textContent = draws;
         displayRounds.textContent = rounds;
 
+        if (wins === 5) {
+            displayResult.textContent = `CONGRATULATIONS, you won five out of ${rounds} games and are victorious!`;
+        }
+        if (losses === 5) {
+            displayResult.textContent = `OH NO, you lost ${losses} (and ${draws} where draw) and are, unfortunately a bit of a loser. Rematch?`;
+        } 
+        if (wins === 5 || losses === 5) {
+            displayResult.style.cssText = 'border: 2px dashed red; background-color: pink; padding: 10px;';
+            document.getElementById("selection").style.display = "none"; //HIDES BUTTONS - ADD NEW with a "reset" function!!!
+        }
+
     });
 });
-
-
 
 function getComputerChoice () { //function for random computer choice
     const randomNumber = Math.ceil(Math.random() * 3);
@@ -53,7 +69,7 @@ function getComputerChoice () { //function for random computer choice
         default:
             return false; //avoiding infinite loops..
     }
-}
+};
 
 function playRound (playerChoice, computerChoice) {
 
@@ -69,47 +85,22 @@ function playRound (playerChoice, computerChoice) {
     } else {
         return false;
     }
-}
+};
 
-function playGame() { //CURRENTLY NOT USED!
-    let wins = 0;
-    let losses = 0;
-    let draws = 0;
+function reset () {
 
-    for (let i = 0; i < 5; i++) {
-        //prompt for player choice, toUpperCase for string comparison
-        //const computerChoice = getComputerChoice();
-        //const playerChoice = prompt('Rock, Paper och Scissors?', 'rock').toUpperCase(); 
+    rounds = 0;
+    wins = 0;
+    losses = 0;
+    draws = 0;
 
-        //log player and computer messages for error check
-        console.log(`You chose ${playerChoice}.`); 
-        console.log(`Computer "chose" ${computerChoice}.`);
-        
-        //Count result
-        const result = playRound(playerChoice, computerChoice);
-        if (result === 'draw') {
-            console.log('It is a draw');
-            draws ++;
-        } else if (result === 'win') {
-            console.log('You won the round!');
-            wins ++;
-        } else {
-            console.log('You lost the round!');
-            losses ++;
-        }
-    }
-    
-    console.log(`Number of wins: ${wins}`);
-    console.log(`Number of losses: ${losses}`);
-    console.log(`Number of draws: ${draws}`);    
+    displayResult.textContent = 'Well did you win?';   
+    displayWins.textContent = wins;
+    displayLosses.textContent = losses;
+    displayDraws.textContent = draws;
+    displayRounds.textContent = rounds;
 
-    //print winner
-    if (wins > losses) {
-        console.log(`CONGRATULATIONS, you won ${wins} out of five games and are victorious!`);
-    } else if (losses > wins) {
-        console.log(`OH NO, you lost ${losses} (and ${draws} where draw) out of five games and are, unfortunately a bit of a loser. Rematch?`);
-    } else {
-        console.log('Well how about that, a tie. What an anticlimax. Rematch maybe?');
-    } 
+    displayResult.style.cssText = '';
+    document.getElementById("selection").style.display = "block";
 
-}
+};
